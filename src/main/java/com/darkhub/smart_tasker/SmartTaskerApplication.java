8bfea6507bbe713cb0dead901afc20ea270ae3c2
YamiDarknezz <erick.plasencia54@gmail.com>
2025-07-12 22:14:03 -0500

    Creacion del proyecto

---
+++ src/main/java/com/darkhub/smart_tasker/SmartTaskerApplication.java
@@ -0,0 +1,13 @@
+package com.darkhub.smart_tasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTaskerApplication.class, args);
+	}
+
+}
