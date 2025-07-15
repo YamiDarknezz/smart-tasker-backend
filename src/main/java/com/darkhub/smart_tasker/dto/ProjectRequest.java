6e6a43064cba5d0133d7abc353ba1f1085cd8490
YamiDarknezz <erick.plasencia54@gmail.com>
2025-07-15 18:30:44 -0500

    fix warnnins enums

--- src/main/java/com/darkhub/smart_tasker/dto/ProjectRequest.java
+++ src/main/java/com/darkhub/smart_tasker/dto/ProjectRequest.java
@@ -1,11 +1,9 @@
 package com.darkhub.smart_tasker.dto;
 
-import com.darkhub.smart_tasker.entity.enums.ProjectStatus;
 import com.darkhub.smart_tasker.entity.enums.ProjectType;
 import lombok.Data;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
 @Data
 public class ProjectRequest {
